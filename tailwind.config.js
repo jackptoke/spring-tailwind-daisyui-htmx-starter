981281bde00d30f98bc82685326b789517ed761e
Jack Toke <54049012+jackptoke@users.noreply.github.com>
2024-05-03 19:10:00 +1000

    Wednesday May 3, 2024 Evening

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/main/resources/templates/**/*.html"],
-    daisyui: {
-      themes: ["light", "dark", {
-          ame_theme: {
-              "primary":"#30336b",
-              "secondary": "#f0932b",
-              "accent": "#ff7979",
-              "neutral": "#f0932b",
-              "base-100": "#f5f6fa"
-          }
-      }]
-    },
+      theme: {
+        colors: {
+            "primary":"#30336b",
+            "secondary": "#f0932b",
+            "accent": "#ff7979",
+            "neutral": "#f0932b",
+            "base-100": "#f5f6fa"
+        }
+      }
+    ,
   theme: {
     extend: {},
   },
   plugins: [
       require("@tailwindcss/typography"),
-      require('@tailwindcss/forms'),
-      require("daisyui")],
+      require('@tailwindcss/forms')]
 }
 
